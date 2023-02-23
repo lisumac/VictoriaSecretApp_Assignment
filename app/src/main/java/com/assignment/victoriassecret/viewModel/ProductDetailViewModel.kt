3165f1e2c2683c8f3f28083abaff702d43c718ef
lisumac <lisadasmohapatra414@gmail.com>
2023-02-23 07:11:55 +0530

    added product details

---
+++ app/src/main/java/com/assignment/victoriassecret/viewModel/ProductDetailViewModel.kt
@@ -0,0 +1,12 @@
+package com.assignment.victoriassecret.viewModel
+
+import androidx.lifecycle.ViewModel
+import com.assignment.victoriassecret.data.repository.ProductListRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ProductDetailViewModel @Inject constructor(): ViewModel() {
+
+
+}
