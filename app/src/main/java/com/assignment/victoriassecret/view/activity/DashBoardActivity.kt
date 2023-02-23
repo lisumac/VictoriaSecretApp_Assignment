42f5a7fc1865225ff184672abfc0f2ecaa54189b
lisumac <lisadasmohapatra414@gmail.com>
2023-02-23 17:03:31 +0530

    added product details

--- app/src/main/java/com/assignment/victoriassecret/view/activity/DashBoardActivity.kt
+++ app/src/main/java/com/assignment/victoriassecret/view/activity/DashBoardActivity.kt
@@ -20,15 +20,12 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class DashBoardActivity : AppCompatActivity() {
     lateinit var dashboardBinding: ActivityDashBoardBinding
-    var tabnames: ArrayList<String> = ArrayList()
-    private var indicatorWidth = 0
     lateinit var navController: NavController
     private val dashBoardViewModel: DashBoardViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initDataBindingAndViewModel()
-       /* setupViewPager()
-        setupTabLayout()*/
+
 
 
     }
@@ -43,60 +40,5 @@ class DashBoardActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
-  /*  private fun setupTabLayout() {
-        // tabnames.add(0,"TabName")
-        tabnames.add(0, "ProductList")
-        tabnames.add(1, "Profile")
-        TabLayoutMediator(dashboardBinding.tabLayout, dashboardBinding.viewPager) { tab, position ->
-            tab.text = tabnames[position]
-        }.attach()
-        val adapter = ViewPagerAdapter(this, 2)
-        dashboardBinding.viewPager.adapter = adapter
-        dashboardBinding.tabLayout.post(Runnable {
-            indicatorWidth = dashboardBinding.tabLayout.width / dashboardBinding.tabLayout.tabCount
 
-            //Assign new width
-            val indicatorParams =
-                dashboardBinding.indicator.layoutParams as FrameLayout.LayoutParams
-            indicatorParams.width = indicatorWidth
-            dashboardBinding.indicator.layoutParams = indicatorParams
-        })
-
-
-        dashboardBinding.viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
-            override fun onPageScrolled(
-                position: Int,
-                positionOffset: Float,
-                positionOffsetPixels: Int
-            ) {
-                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
-
-
-                val params = dashboardBinding.indicator.layoutParams as FrameLayout.LayoutParams
-                //Multiply positionOffset with indicatorWidth to get translation
-                val translationOffset: Float = (positionOffset + position) * indicatorWidth
-                params.leftMargin = translationOffset.toInt()
-                dashboardBinding.indicator.layoutParams = params
-            }
-
-        })
-
-
-    }
-
-    private fun setupViewPager() {
-        val adapter = ViewPagerAdapter(this, 2)
-        dashboardBinding.viewPager.adapter = adapter
-
-
-    }
-
-    override fun onBackPressed() {
-        val viewPager = dashboardBinding.viewPager
-        if (viewPager.currentItem == 0) {
-            super.onBackPressed()
-        } else {
-            viewPager.currentItem = viewPager.currentItem - 1
-        }
-    }*/
 }
